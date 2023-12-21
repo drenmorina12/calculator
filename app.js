1e9be364da630c41b3870674b96f3dd8e25ffad0
Dren Morina <drenmorina321@gmail.com>
2023-12-21 17:23:29 +0100

    Create operate() function and add event listeners to operations

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let operator1, operator2, operator;
+const operators = document.querySelectorAll(".operator");
+
+let number1, number2, operator;
 
 function add(x, y) {
   return x + y;
@@ -15,3 +17,28 @@ function multiply(x, y) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(number1, number2, operator) {
+  switch (operator) {
+    case "+":
+      return add(number1, number2);
+      break;
+    case "-":
+      return sub(number1, number2);
+      break;
+    case "*":
+      return multiply(number1, number2);
+      break;
+    case "/":
+      return divide(number1, number2);
+      break;
+    default:
+      break;
+  }
+}
+
+operators.forEach((op) => {
+  op.addEventListener("click", () => {
+    operator = op.value;
+  });
+});
