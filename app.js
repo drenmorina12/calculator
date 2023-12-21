c8148c2618e4ae97b1eee31ea6908525956eeb0e
Dren Morina <drenmorina321@gmail.com>
2023-12-21 17:03:34 +0100

    Create +,-,*,/ functions

---
+++ app.js
@@ -0,0 +1,17 @@
+let operator1, operator2, operator;
+
+function add(x, y) {
+  return x + y;
+}
+
+function sub(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(a, b) {
+  return a / b;
+}
